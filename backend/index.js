e549eef814fa8d9eb0e5c6150d913f6abaa92308
Fiza Ahmad Baizuri <fizaahmadbaizuri@gmail.com>
2024-12-16 04:25:17 +0700

    added form to create new goal, added new functionality to create new goal in backend

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,7 @@ app.get('/', (req, res) => {
     res.send('Get request to the homepage')
 })
 
+// Get all goals
 app.get('/goals', async (req, res) => {
     const goals = await getGoals()
     res.json({ result: goals })
@@ -20,6 +21,18 @@ app.get('/goal/:id', async (req, res) => {
     const id = req.params.id
     const goal = await getGoal(id)
     res.json({ result: goal })
+})
+
+// Create new goal
+app.post('/creategoal', async (req, res) => {
+    try {
+        const { title, description } = req.body
+        const newGoal = await createGoal(title, description, 0)
+        res.json({ result: newGoal })
+    } catch (error) {
+        console.log('Error creating goal', error)
+        res.status(500).json({ error: 'An error occured while creating a goal' })
+    }
 })
 
 app.listen(8000, () => {
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -3,6 +3,8 @@ import { ref } from 'vue'
 import { RouterLink } from 'vue-router';
 import axios from 'axios'
 
+
+// Get all goals
 const allGoals = ref([])
 const emojis = ref(['😁', '😭', '😎', '🤖', '😽', '🤩'])
 
@@ -18,12 +20,55 @@ async function getAllGoals() {
   }
 }
 getAllGoals()
+
+
+// Add new goal
+const modalState = ref(false)
+
+const addNew = (() => {
+  modalState.value = true
+})
+
+const closeModal = (() => {
+  modalState.value = false;
+})
+
+const goalTitle = ref('')
+const goalDescription = ref('')
+const newGoal = ref(null)
+
+async function addNewGoal() {
+  try {
+    const response = await axios.post('http://localhost:8000/creategoal', {
+      title: goalTitle.value,
+      description: goalDescription.value
+    })
+    newGoal.value = response.data.result
+    modalState.value = false;
+    await getAllGoals()
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
 </script>
 
 <template>
   <main>
     <div class="container">
       <div class="card-container">
+        <div class="add-new" @click="addNew()">+</div>
+        <div class="modal" v-if="modalState">
+          <button class="close-modal" @click="closeModal()">X</button>
+          <div class="modal-content">
+            <form class="new-goal-form" @submit.prevent>
+              <input type="text" placeholder="title" v-model="goalTitle">
+              <input type="text" placeholder="description" v-model="goalDescription">
+              <button type="button" @click="addNewGoal">Add New Goal</button>
+            </form>
+          </div>
+        </div>
         <div class="card" v-for="goal in allGoals" :key="goal.id">
           <RouterLink :to='`/details/${goal.id}`'>
             <h3>{{ goal.emoji }} {{ goal.title }}</h3>
@@ -50,7 +95,6 @@ a {
 }
 
 .container {
-  // background-color: red;
   margin-top: 20px;
   margin-left: 80px;
   margin-right: 80px;
@@ -59,6 +103,108 @@ a {
     display: flex;
     flex-wrap: wrap;
     justify-content: center;
+    position: relative;
+
+    .add-new {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      position: fixed;
+      bottom: 50px;
+      right: 70px;
+      background-color: #92FE9D;
+      height: 50px;
+      width: 50px;
+      border-radius: 50px;
+      color: white;
+      cursor: pointer;
+      background: linear-gradient(90deg, #00C9FF 0%, #92FE9D 100%);
+      transition: ease-in-out 150ms;
+      box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+
+      &:hover {
+        transform: scale(1.1);
+      }
+    }
+
+    .modal {
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: linear-gradient(90deg, #00C9FF 0%, #92FE9D 100%);
+      display: flex;
+      align-items: center;
+      justify-content: center;
+
+      .close-modal {
+        position: fixed;
+        top: 20px;
+        right: 30px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        cursor: pointer;
+        font-family: "Poppins", sans-serif;
+        border: none;
+        width: 50px;
+        height: 50px;
+        padding: 20px;
+        border-radius: 50px;
+        transition: ease-in-out 150ms;
+        box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+
+        &:hover {
+          transform: scale(1.1);
+        }
+      }
+
+      .modal-content {
+        display: flex;
+        justify-content: center;
+        width: 100%;
+        padding: 50px;
+        animation: moveUp 0.3s cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
+
+        .new-goal-form {
+          height: 12rem;
+          width: 30rem;
+          padding: 15px;
+          border-radius: 15px;
+          box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+          background: linear-gradient(90deg, #e3ffe7 0%, #d9e7ff 100%);
+
+          input {
+            font-family: 'Poppins', sans-serif;
+            width: 100%;
+            padding: 10px;
+            margin-top: 10px;
+            border-radius: 15px;
+            border: none;
+          }
+
+          button {
+            font-family: 'Poppins', sans-serif;
+            border: none;
+            color: white;
+            padding: 7px;
+            width: 8rem;
+            border-radius: 10px;
+            cursor: pointer;
+            background: linear-gradient(90deg, #00C9FF 0%, #92FE9D 100%);
+            float: right;
+            margin-top: 15px;
+            transition: ease-in-out 150ms;
+            box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+
+            &:hover {
+              transform: scale(1.1);
+            }
+          }
+        }
+      }
+    }
 
     .card {
       width: 30rem;
@@ -79,9 +225,28 @@ a {
         cursor: pointer;
         background: linear-gradient(90deg, #00C9FF 0%, #92FE9D 100%);
         float: right;
+        transition: ease-in-out 150ms;
         box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
+
+
+        &:hover {
+          transform: scale(1.1);
+        }
       }
     }
+  }
+}
+
+@keyframes moveUp {
+  0% {
+    transform: scale(0);
+    opacity: 0;
+    border-radius: 15px;
+  }
+
+  100% {
+    transform: scale(1);
+    opacity: 1;
   }
 }
 </style>
