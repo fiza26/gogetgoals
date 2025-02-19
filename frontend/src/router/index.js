b71d853ddd2f729a511cbaba6ca163966060806f
Fiza Ahmad Baizuri <fizaahmadbaizuri@gmail.com>
2025-02-19 23:49:09 +0700

    added profile page

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import DetailsView from '@/views/DetailsView.vue'
 import LoginView from '@/views/LoginView.vue'
+import ProfileView from '@/views/ProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,11 @@ const router = createRouter({
       path: '/details/:id',
       name: 'details',
       component: DetailsView,
+    },
+    {
+      path: '/profile',
+      name: 'profile',
+      component: ProfileView,
     }
   ],
 })
